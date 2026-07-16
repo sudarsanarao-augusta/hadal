ea083177466ddd9b87874720679450b883d7702c
sudarsanarao-augusta <sudarsana.rao@augustahitech.com>
2026-07-16 09:44:26 +0530

    [ADDED] playwright/locators/locators.json — locators file

---
+++ sudarsana.rao@augustahitech.com/fixtures/3182107a-0909-47bb-b49b-bd2e7772d926.selectors.ts
@@ -0,0 +1,6 @@
+{
+  "api_call_unread-count": "api_call_unread-count",
+  "api_call_in-progress-count": "api_call_in-progress-count",
+  "api_call_in-progress": "api_call_in-progress",
+  "api_call_notifications": "api_call_notifications"
+}
