b3bd9f7e64bc30dec0fd19f927f2a72f37064e94
sudarsanarao-augusta <sudarsana.rao@augustahitech.com>
2026-07-16 09:44:26 +0530

    [ADDED] playwright/locators/locators.json — locators file

---
+++ sudarsana.rao@augustahitech.com/fixtures/49886866-31fa-4292-a154-65f2cee81742.selectors.ts
@@ -0,0 +1,12 @@
+{
+  "usernameInput": "#user-name",
+  "passwordInput": "#password",
+  "loginButton": "#login-button",
+  "addToCartSauceLabsBackpack": "#add-to-cart-sauce-labs-backpack",
+  "addToCartSauceLabsBoltTShirt": "#add-to-cart-sauce-labs-bolt-t-shirt",
+  "addToCartSauceLabsBikeLight": "#add-to-cart-sauce-labs-bike-light",
+  "shoppingCartLink": "[data-test=\"shopping-cart-link\"]",
+  "checkoutButton": "#checkout",
+  "continueButton": "#continue",
+  "cancelButton": "#cancel"
+}
